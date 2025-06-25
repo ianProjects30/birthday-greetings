24781080a116272a10e359d7e6409c34ceda38f8
Ian Morete <christianmorete3@gmail.com>
2025-06-25 23:57:11 +0800

    m

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/birthday-greetings/', // 👈 This line is important
+  base: '/birthday-greetings/', // <- must match your GitHub repo name exactly
   plugins: [react()],
-})
+});
